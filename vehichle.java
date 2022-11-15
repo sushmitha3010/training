2c913932010e536487f1b4e81db78c7d2a8f1319
sushmitha3010 <118182610+sushmitha3010@users.noreply.github.com>
2022-11-15 15:21:55 +0530

    Add files via upload
    files via upload

---
+++ vehichle.java
@@ -0,0 +1,11 @@
+package com.chainsys.novbtch;
+
+public class vehichle {
+
+		String vehichleName;
+		int noOfVehichle;
+		String vehichleBrandName;
+		Boolean ifVehichleIsTwoWheeler;
+		int numberPlate;// TODO Auto-generated method stub
+
+	}
